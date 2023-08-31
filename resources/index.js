4d380b293cf4700a2820fa8720c0cbca0e37e817
Joshua <joshuahsokolowski@gmail.com>
2023-08-31 03:55:06 -0400

    Logic completed

--- resources/index.js
+++ resources/index.js
@@ -1,10 +1,30 @@
+var timer = null;
 function timerStart() {
-    var targetDate = document.getElementById("date_input").valueAsNumber;
-    var thisDate = Date.now();
-    var timerText = document.getElementById("timer");
-    console.log(thisDate - targetDate);
-    if (targetDate - thisDate <= 0) {
+    var targetDate = new Date(document.getElementById("date_input").valueAsNumber);
+    var thisDate = new Date();
+    if (targetDate < thisDate) {
         return;
     }
-    timerText.innerHTML = "Clicked.";
+    if (timer !== null) {
+        clearInterval(timer);
+    }
+    timer = setInterval(function () {
+        countdown(targetDate, thisDate);
+    }, 1000);
+}
+function countdown(target, today) {
+    var dayText = document.getElementById("days");
+    var hourText = document.getElementById("hours");
+    var minText = document.getElementById("minutes");
+    var secText = document.getElementById("seconds");
+    var days, hours, minutes, seconds;
+    today = new Date();
+    var remaining = target.valueOf() - today.valueOf();
+    dayText.innerHTML = Math.floor(remaining / 86000000).toString() + " days";
+    remaining = remaining % 86000000;
+    hourText.innerHTML = Math.floor(remaining / 3600000).toString() + " hours";
+    remaining = remaining % 3600000;
+    minText.innerHTML = Math.floor(remaining / 60000).toString() + " minutes";
+    remaining = remaining % 60000;
+    secText.innerHTML = Math.floor(remaining / 1000).toString() + " seconds";
 }
--- resources/index.ts
+++ resources/index.ts
@@ -1,15 +1,38 @@
-
-
+let timer: number = null;
 
 function timerStart() {
-    let targetDate = (document.getElementById("date_input") as HTMLInputElement).valueAsNumber;
-    let thisDate = Date.now();
-    let timerText = document.getElementById("timer");
-    console.log(thisDate - targetDate);
+    let targetDate = new Date((document.getElementById("date_input") as HTMLInputElement).valueAsNumber);
+    let thisDate = new Date();
 
-    if (targetDate - thisDate <= 0) {
+    if (targetDate < thisDate) {
         return;
     }
 
-    timerText.innerHTML = "Clicked.";
+    if (timer !== null) {
+        clearInterval(timer);
+    }
+
+    timer = setInterval(function() {
+        countdown(targetDate, thisDate);
+    }, 1000);
+    
+}
+
+function countdown(target: Date, today: Date) {
+    let dayText = document.getElementById("days");
+    let hourText = document.getElementById("hours");
+    let minText = document.getElementById("minutes");
+    let secText = document.getElementById("seconds");
+    let days: number, hours: number, minutes: number, seconds: number;
+
+    today = new Date();
+    let remaining = target.valueOf() - today.valueOf();
+
+    dayText.innerHTML = Math.floor(remaining / 86000000).toString() + " days";
+    remaining = remaining % 86000000;
+    hourText.innerHTML = Math.floor(remaining / 3600000).toString() + " hours";
+    remaining = remaining % 3600000;
+    minText.innerHTML = Math.floor(remaining / 60000).toString() + " minutes";
+    remaining = remaining % 60000;
+    secText.innerHTML = Math.floor(remaining / 1000).toString() + " seconds";
 }
